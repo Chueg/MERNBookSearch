9515a805ab40e0ebdcc7544d29b2cc4ce4a0f31b
Choogi <drewtex@gmail.com>
2022-11-28 15:50:37 -0600

    apollo server

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,38 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type User {
+    _id: ID!
+    username: String!
+    email: String!
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+  type Book {
+    bookId: String!
+    authors: [String]
+    description: String!
+    title: String!
+    image: String
+    link: String
+
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+
+  type Query {
+    me: [User]
+  }
+
+  type Mutation {
+    createMatchup(tech1: String!, tech2: String!): Matchup
+    createVote(_id: String!, techNum: Int!): Matchup
+  }
+`;
+
+module.exports = typeDefs;
